08c58fbed2b33fdde516fda87320a396bb885a1f
Jeroen Sangers <github@jeroensangers.com>
2014-05-22 13:57:14 +0200

    Fix Google Analytics tracking

--- js/mt.js
+++ js/mt.js
@@ -1551,7 +1551,6 @@ function mtInitCommenter () {
 function trackOutboundLink(link, category, action) {
 	try { 
 		ga('send', 'event', category, action);
-		_gaq.push(['_trackEvent', category , action]); 
 	} catch(err){}
 	setTimeout(function() {
 		document.location.href = link.href;
